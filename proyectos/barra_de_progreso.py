90bd9ceef046f4f7adf0ebe240045d9e59cae74b
SrJesurun <mariojesurun2003@gmail.com>
2025-06-10 11:21:42 -0400

    commit

---
+++ proyectos/barra_de_progreso.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+ancho = 20
+for i in range(ancho + 1):
+    porcentaje = int(i / ancho * 100)
+    
+    os.system("cls")
+    
+    barra = "#" * i + "-" * (ancho - i)
+    print(f"{barra}{porcentaje}%")
+    
+    time.sleep(0.1)
