f6ca764d11c4906e3a667a6919c6a85fceca6632
SrJesurun <mariojesurun2003@gmail.com>
2025-08-20 14:25:52 -0400

    commit

---
+++ proyectos/numero_primo.py
@@ -0,0 +1,15 @@
+def es_primo(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+numero = int(input("Introduce un numero: "))
+
+if es_primo(numero):
+    print(f"El numero {numero} es primo.")
+    
+else:
+    print(f"El numero {numero} No es primo.")
